d068e096c7f42edc0468e310067af33269f85f21
benlevine1 <43486139+benlevine1@users.noreply.github.com>
2018-10-26 12:41:00 -0700

    rootJS-final-project
    I'm sure most of this is no good, but I wanted to get some feedback to see if I was headed in the right direction at all.

--- includes/main.js
+++ includes/main.js
@@ -23,27 +23,55 @@ function initiateApp(){
 	/*advanced: add jquery sortable call here to make the gallery able to be sorted
 		//on change, rebuild the images array into the new order
 	*/
+	$('#gallery').sortable({
+
+	});
+
 	makeGallery(pictures);
 	addModalCloseHandler();
 }
-function makeGallery(imageArray){
-	//use loops and jquery dom creation to make the html structure inside the #gallery section
-
+function makeGallery(imageArray) {
+    //use loops and jquery dom creation to make the html structure inside the #gallery section
+    var x = 1;
+	while (x < 11){
+    x++;
+    if (x === 4 || x === 5 || x === 6 || x === 7){
+    	var figure = $('<figure>', {
+    		"class": "imageGallery col-xs-12 col-sm-6 col-md-4",
+			style: "background-image:url(images/landscape-" + x + ".jpg);",
+		)};
+    	var figcaption = $('<figcaption>', {
+    		text: 'landscape-' + x + '.jpg'
+    	});
+    };
+	};
+	figure.append(figcaption);
+};
 	//create a loop to go through the pictures
 		//create the elements needed for each picture, store the elements in variable
 
 		//attach a click handler to the figure you create.  call the "displayImage" function.  
-
+figure.click(displayImage(){
+    $('#gallery').append(figure);
+});
 		//append the element to the #gallery section
 
 }
 
 function addModalCloseHandler(){
 	//add a click handler to the img element in the image modal.  When the element is clicked, close the modal
+$('.modal img').click(function(){
+	$('.modal').modal('hide')
+});
 	//for more info, check here: https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp	
 }
 
 function displayImage(){
+	getImageUrl function(){
+        var url = $('figure style');
+        url = url.replace
+	}
+
 	//find the url of the image by grabbing the background-image source, store it in a variable
 	//grab the direct url of the image by getting rid of the other pieces you don't need
 
